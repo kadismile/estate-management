54c089514c8ced583b65c6eadcf407db4bea25de
kadismile <bbrianking319@gmail.com>
2020-02-18 09:07:20 +0100

    added models controllers and schemas

--- middleware/errors.ts
+++ middleware/errors.ts
@@ -1,21 +1,27 @@
-import { Request, Response, NextFunction, ErrorRequestHandler } from 'express'
+import {Request, Response, NextFunction, ErrorRequestHandler} from 'express'
+
 const ErrorResponse = require('../utils/errorResponse');
 
 // @ts-ignore
-const errorHandler = (err:ErrorResponse, req:Request, res:Response, next: NextFunction)=> {
-  let error = {...err};
-  error.message = err.message;
+const errorHandler = (err: ErrorResponse, req: Request, res: Response, next: NextFunction) => {
+    let error = {...err};
+    error.message = err.message;
 
-  console.log("ERROR_OOOOOOO ", err);
+    console.log("ERROR_OOOOOOO ", err);
 
-if (err.type === 'entity.parse.failed') {
-const message = `Wrong Entity as een Parsed`;
-error = new ErrorResponse(message, 404)
-}
- res.status(error.statusCode || 500).json({
-    success: false,
-    error: error.message || 'Server Error'
-  })
+    if (err.type === 'entity.parse.failed') {
+        const message = `Wrong Entity as been Parsed`;
+        error = new ErrorResponse(message, 404)
+    }
+
+    if (err.name === 'SequelizeUniqueConstraintError') {
+        const message = `Unique Entity Error`;
+        error = new ErrorResponse(message, 404)
+    }
+    res.status(error.statusCode || 500).json({
+        success: false,
+        error: error.message || 'Server Error'
+    })
 };
 
 module.exports = errorHandler;
