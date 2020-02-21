7a97a88b5e613fbda1ed1c0fcc89e6b66fbcd368
chibuzc <chibuzc@yahoo.co.uk>
2020-02-21 01:48:25 +0100

    setting up tests for more routes

--- routes/estateAdmin.ts
+++ routes/estateAdmin.ts
@@ -2,7 +2,7 @@ import express, { Request, Response, NextFunction } from 'express'
 const router = express.Router();
 const { createEstateAdmins } = require('../controllers/estateAdmins');
 
-router.route('/create')
+router.route('/:create')
   .post(createEstateAdmins);
 
 
--- test/index-spec.ts
+++ test/index-spec.ts
@@ -4,6 +4,8 @@ import { dropDb } from "../seed/dropDB";
 import "mocha";
 import chai, { expect, assert } from "chai";
 import chaiHttp = require("chai-http");
+import { EstateAdminAttributes } from "../types/estateAdmin";
+import { request } from "express";
 chai.use(chaiHttp);
 
 describe("test", () => {
@@ -14,21 +16,57 @@ describe("test", () => {
 
 describe("Describe the basic nature of what are the series of test cases here", () => {
   it("Creates an estate admin", done => {
+    let newAdmin : EstateAdminAttributes = {
+      id: Math.floor(Math.random() * 100000),
+      name: "johnny doslese",
+      email: "johndalapo@test.com",
+      phoneNumber: "+2348049835094",
+      address: "24 Bansxheer Avenue",
+      estateType: "Block"
+    };
     chai
       .request(server)
       .post("/api/v1/estate-admin/create")
-      .send({
-        id: Math.floor(Math.random() * 100000),
-        name: "johnny doslese",
-        email: "johndalapo@test.com",
-        phoneNumber: "+2348049835094",
-        address: "24 Bansxheer Avenue",
-        estateType: "Block"
-      })
+      .send(newAdmin)
       .then((res: any) => {
         chai.expect(res.status).to.eql(201); // expression which will be true if response status equal to 201
         chai.assert.exists(res.body.data.id); // assertion expression which will be true if id exists
-        chai.expect(res.body.data.name).to.eql("johnny doslese"); // expression which will be true if name equal to john doe
+        chai.expect(res.body.data).to.deep.include(newAdmin); // expression which will be true if the document created contains the body that was sent
+        done();
+      })
+      .catch(done);
+  });
+
+  it("Updates an estate admin", done => {
+    let newAdmin : EstateAdminAttributes = {
+      name: "johnny doese",
+      email: "johnpo@test.com",
+      phoneNumber: "+2348049835094",
+      address: "24 Bansxheer Avenue",
+      estateType: "Block"
+    };
+    chai
+      .request(server)
+      .post(`/api/v1/estate-admin/:id`)
+      .send(newAdmin)
+      .then(( res: any) => {
+        //TODO Confirm that newAdmin.id = req.params.id
+        chai.expect(res.status).to.eql(201); // expression which will be true if response status equal to 201
+        chai.assert.exists(res.body.data.id); // assertion expression which will be true if id exists
+        chai.expect(res.body.data).to.deep.include(newAdmin);// expression which will be true if name equal to john doe
+        done();
+      })
+      .catch(done);
+  });
+
+  it("Gets an estate admin by id", done => {
+    chai
+      .request(server)
+      .get(`/api/v1/estate-admin/:id`)
+      .then(( res: any) => {
+        //TODO Confirm that newAdmin.id = req.params.id
+        chai.expect(res.status).to.eql(201); // expression which will be true if response status equal to 201
+        chai.assert.exists(res.body.data.id); // assertion expression which will be true if id exists
         done();
       })
       .catch(done);
